3d42dbbd65b69704b60e40b515eedaea0e1b0f47
Armando Paulo <armando.paulo@walmart.com>
2020-02-22 16:02:33 -0300

    add react-weathericons library

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -5,7 +5,10 @@ import WeatherTemperature from "./WeatherTemperature";
 const WeatherData = () => (
     <div>
         <WeatherTemperature/>
-        <WeatherExtraInfo/>
+        <WeatherExtraInfo
+            humidity={80}
+            wind={"10 m/s"}
+        />
     </div>
 )
 
--- src/components/WeatherLocation.js
+++ src/components/WeatherLocation.js
@@ -4,7 +4,9 @@ import WeatherData from "./WeatherData";
 
 const WeatherLocation = () => (
     <div>
-        <Location/>
+        <Location
+        city = {"Buenos Aires"}
+        />
         <WeatherData/>
     </div>
 )
