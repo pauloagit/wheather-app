a381c61cd9518ae4b39f1d2c5406f4be1d08a294
Armando Paulo <armando.paulo@walmart.com>
2020-02-23 18:46:58 -0300

    Transformando un Functional component en uns Class component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Component} from "react"
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import './styles.css'
@@ -13,15 +13,20 @@ const data = {
     wind: '10 m/s'
 }
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location
-        city = {"Caracas"}
-        />
-        <WeatherData
-            data={data}
-        />
-    </div>
-)
+class WeatherLocation extends Component {
+
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location
+                    city = {"Caracas"}
+                />
+                <WeatherData
+                    data={data}
+                />
+            </div>
+        )
+    }
+}
 
 export default WeatherLocation
