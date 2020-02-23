9c1fb4643328bbedd92ea4b6f2adc1db887c3099
Armando Paulo <armando.paulo@walmart.com>
2020-02-23 18:30:44 -0300

    Doble destructuring

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,13 +2,25 @@ import React from "react"
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import './styles.css'
+import {
+    SUN,
+} from "../../constants/weathers";
+
+const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s'
+}
 
 const WeatherLocation = () => (
     <div className="weatherLocationCont">
         <Location
         city = {"Caracas"}
         />
-        <WeatherData/>
+        <WeatherData
+            data={data}
+        />
     </div>
 )
 
