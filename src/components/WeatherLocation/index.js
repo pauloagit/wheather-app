9a2e5bf928b56258b5188c051de002510f58b521
Armando Paulo <armando.paulo@walmart.com>
2020-02-26 21:39:03 -0300

    services layer

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,16 +1,14 @@
 import React, {Component} from "react"
-import Location from "./Location";
-import WeatherData from "./WeatherData";
+import Location from "./Location"
+import transformWeather from "../../services/transformWeather";
+import WeatherData from "./WeatherData"
 import './styles.css'
 import {
     SUN,
     WINDY
-} from "../../constants/weathers";
+} from "../../constants/weathers"
+import {api_weather} from '../../constants/api_url'
 
-const location = 'Buenos Aires,ar'
-const api_key = 'b1a7014557729a3630109631832fdd59';
-const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather'
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`
 
 const data = {
     temperature: 5,
@@ -30,31 +28,12 @@ class WeatherLocation extends Component {
         }
     }
 
-    getWeatherState = weather_data => {
-        return SUN
-    }
-
-    getData = weather_data => {
-        const { humidity, temp } = weather_data.main
-        const { speed } = weather_data.wind
-        const weatherState = this.getWeatherState()
-
-        const data = {
-            humidity,
-            temperature: temp,
-            weatherState,
-            wind: `${speed} m/s`,
-        }
-
-        return data
-    }
-
     handleUpdateClick = () => {
 
         fetch(api_weather).then( resolve => {
             return resolve.json()
         }).then(data =>{
-            const newWeather = this.getData(data)
+            const newWeather = transformWeather(data)
             this.setState({
                 data: newWeather,
             })
