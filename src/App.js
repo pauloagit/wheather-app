af3bc9c055afcb88b8c760c0b83acded91f1fa8a
Armando Paulo <armando.paulo@walmart.com>
2020-03-08 17:17:19 -0300

    Import and use Paper, AppBar, Typography and Toolbar of @material-ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import Paper from '@material-ui/core/Paper'
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
 import { Grid, Col, Row } from "react-flexbox-grid";
 import LocationList from "./components/LocationList";
 import './App.css';
@@ -22,7 +26,13 @@ class App extends Component {
         return (
             <Grid>
                 <Row>
-                    Titulo
+                    <AppBar position="sticky">
+                        <Toolbar>
+                            <Typography variant='subtitle1' color='inherit'>
+                                Weather App
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
                 </Row>
                 <Row>
                     <Col xs={12} md={6}>
@@ -32,7 +42,9 @@ class App extends Component {
                         />
                     </Col>
                     <Col xs={12} md={6}>
-                        <div className="details"></div>
+                        <Paper elevation={4}>
+                            <div className="details"></div>
+                        </Paper>
                     </Col>
                 </Row>
             </Grid>
