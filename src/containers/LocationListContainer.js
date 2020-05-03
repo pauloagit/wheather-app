ebb37512b39cf560c749f255fda7c7cc71d4428f
Armando Paulo <armando.paulo@walmart.com>
2020-05-03 16:01:27 -0400

    End class 121 - Creación de container LocationListContainer [Refactor]

---
+++ src/containers/LocationListContainer.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import LocationList from "../components/LocationList"
+import {setCity} from "../actions"
+import {connect} from "react-redux"
+
+class LocationListContainer extends Component {
+
+    handleSelectionLocation = (city) => {
+        this.props.setCity(city)
+    }
+
+    render() {
+        const { cities } = this.props
+        return (
+            <LocationList
+                cities={cities}
+                onSelectedLocation={this.handleSelectionLocation}
+            />
+        )
+    }
+}
+
+LocationListContainer.propTypes = {
+    setCity: PropTypes.func.isRequired,
+    cities: PropTypes.array.isRequired
+}
+
+const mapDispatchToProps = dispatch => ({
+    setCity: value => dispatch(setCity(value))
+})
+
+export default connect(null, mapDispatchToProps)(LocationListContainer)
+
