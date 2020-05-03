38dd8b24c436103b3002a49fecf1475f2040589e
Armando Paulo <armando.paulo@walmart.com>
2020-05-03 19:17:36 -0400

    End class 133 - use Middlware redux - middlware thunk - combineReducers - compose - applyMiddleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,30 @@
-export const SET_CITY = 'SET_CITY'
+import getUrlForecastByCity from "../services/getUrlForecastByCity";
+import transformForecast from "../services/transformForecast";
 
-export const setCity = payload => ({type: SET_CITY, payload})
+export const SET_CITY = 'SET_CITY'
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA'
+
+const setCity = payload => ({type: SET_CITY, payload})
+const setForecastData = payload => ({type: SET_FORECAST_DATA, payload})
+
+export const setSelectedCity = payload => {
+    return dispatch => {
+        const url_forecast = getUrlForecastByCity(payload)
+
+        // Activar en el estado un indicador de busqueda de datos
+        dispatch(setCity(payload))
+
+        return fetch(url_forecast).then(
+            data => data.json()
+        ).then(
+            weather_data => {
+                const forecastData = transformForecast(weather_data)
+                console.log('forecastData', forecastData)
+
+                //Modificar el estado con el resultado de la promise (fetch)
+                dispatch(setForecastData({city: payload, forecastData}))
+
+            }
+        )
+    }
+}
